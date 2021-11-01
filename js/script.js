37a211e827f1c97595c99cfde2186e48923d3bfa
YasmineBB <y.bourhabi@gmail.com>
2021-11-01 13:31:17 +0000

    Add social media icons

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let tacos = 'nice';
+tacos = 'delicious';
+
+let person = {
+    name: 'Caleb',
+    age: 93,
+    favFood: 'Pizza',
+
+    tap: function () {
+        console.log('Woo!');
+    }
+}
+
+person.tap();
---
+++ js/script2.js
@@ -0,0 +1,11 @@
+document.getElementById('button').onclick = function () {
+    document.getElementById('confirm').innerHTML = "Order placed, check email for confirmation";
+    document.getElementById('button').style.display = 'none';
+}
+
+
+let name = 30;
+console.log(age);
+
+const message = 'message';
+console.log(message);
